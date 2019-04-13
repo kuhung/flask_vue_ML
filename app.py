97b67e170ad78135b6e793a041229046c7f02382
kuhung <kuhung@foxmail.com>
2019-04-13 07:46:25 +0000

    commited flask_mongo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_cors import CORS
 from flair.models import TextClassifier
